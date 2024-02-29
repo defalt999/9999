c32baf0ce27d5154edde572435c156783bfd38d2
Denis-Cătălin BRATOSIN <denis.bratosin@student.unitbv.ro>
2024-02-29 15:17:48 +0200

    goober

--- ploae.cpp
+++ ploae.cpp
@@ -1,11 +1,48 @@
-
 #include <iostream>
 using namespace std;
 
+class carte {
+private:
+	string nume;
+	string autor;
+	int pagini;
+
+
+
+
+public:
+	void init(string numes) {
+		carte numes;
+
+	}
+
+	void setNume(string unNume) {
+		nume = unNume;
+
+	}
+	void setAutor(string unAutor) {
+		autor = unAutor;
+	}
+	void setPg(int nrPagini) {
+		pagini = nrPagini;
+	}
+	void print() {
+		cout << "Titlu: " << nume << endl;
+		cout << "Autor: " << autor << endl;
+		cout << "Nr pagini: " << pagini << endl;
+	}
+};
+
+
+
+
+
+
 int main()
 {
-    std::cout << "Test github\n";
-    std::cout << "modificare";
-    std:cout << "deci merge";
+
+
+
 }
+
 
