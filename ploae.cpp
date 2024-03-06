a2a9c52f28454e17217734a74bafcc3cfaebd4ee
Denis-Cătălin BRATOSIN <denis.bratosin@student.unitbv.ro>
2024-03-06 15:41:22 +0200

    yoyoy

--- ploae.cpp
+++ ploae.cpp
@@ -1,63 +1,11 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 using namespace std;
-
-class carte {
-private:
-	string nume;
-	string autor;
-	int pagini;
-
-
-
-
-public:
-
-	void setBook(string unnume, string unautor, int unpgs) {
-		nume = unnume;
-		autor = unautor;
-		pagini = unpgs;
-
-	}
-	void print(ofstream& fisi) {
-		fisi <<  nume << endl;
-		fisi << autor << endl;
-		fisi << pagini << endl;
-		
-	}
-	void consolePrint() {
-		cout <<nume <<endl;
-		cout <<autor <<endl;
-		cout <<pagini <<endl;
-	}
-};
-
-
-
-
-
-
+ifstream fisier("citeste.txt");
 int main()
 {
 	
-	ofstream fisier("cartsi.txt", std::ios::app);
-	string nume, autor;
-	int pgs;
-	getline(cin,nume);
-	getline(cin, autor);
-	cin >> pgs;
-	carte book;
-	book.setBook(nume, autor, pgs);
-	book.print(fisier);
-	book.consolePrint();
 	
-
-
-
-
-
-	fisier.close();
 	
 
 }
